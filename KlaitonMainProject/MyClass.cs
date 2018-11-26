d2f54ca7a16d86c9511dd684a4fa53e2426b93ff
Szilvia Orban <rasztaszilvi@gmail.com>
2018-11-27 00:30:57 +0100

    first unit tests

---
+++ KlaitonMainProject/MyClass.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KlaitonUnitTests
+{
+    public class KlaitonProject
+    {
+
+        public enum BorderType { Low, High }
+
+        public enum BorderRange { Day, Month, Year }
+
+        public class BorderModel
+        {
+            public BorderType Type { get; set; }
+            public BorderRange Range { get; set; }
+        }
+
+        public interface IInterface
+        {
+            /// <summary>
+            /// get the low or high value in defined range included the range
+            /// </summary>
+            /// <param name="input">the current time </param>
+            /// <param name="model">the range definition</param>
+            /// <returns></returns>
+            DateTime GetBorderTime(DateTime input, BorderModel model);
+        }
+
+        public class MyClass : IInterface
+        {
+            /// <summary>
+            /// implementation of the IInterface 
+            /// </summary>
+            /// <param name="current"></param>
+            /// <param name="model"></param>
+            /// <returns></returns>
+            public DateTime GetBorderTime(DateTime current, BorderModel model)
+            {
+                switch (model.Type)
+                {
+                    case BorderType.High: return GetLowBorder(current, model.Range);
+                    case BorderType.Low: return GetHighBorder(current, model.Range);
+                }
+                return DateTime.MinValue;
+            }
+
+            public DateTime GetLowBorder(DateTime current, BorderRange range)
+            {
+                switch (range)
+                {
+                    case BorderRange.Day: return new DateTime(current.Year, current.Month, current.Day);
+                    case BorderRange.Month: return new DateTime(current.Year, current.Month, 0);
+                }
+                throw new NotSupportedException("Other values are not supported");
+            }
+
+            public DateTime GetHighBorder(DateTime current, BorderRange range)
+            {
+                switch (range)
+                {
+                    case BorderRange.Day: return new DateTime(current.Year, current.Month, current.Day + 1);
+                    case BorderRange.Year: return new DateTime(current.Year + 1);
+                }
+                throw new NotSupportedException("Other values are not supported");
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/MyUnittest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static KlaitonUnitTests.KlaitonProject;
+
+
+namespace KlaitonMainProjectTest
+{
+    /// <summary>
+    /// Summary description for MyUnittest
+    /// </summary>
+    [TestClass]
+    public class MyUnittest
+    {
+
+        [TestMethod]
+        public void MyClassTest_Create()
+        {
+            IInterface obj = new MyClass();
+            Assert.IsNotNull(obj);
+        }
+
+        //write other unittest to find the bugs 
+
+        [DataRow(BorderRange.Day)]
+        [DataRow(BorderRange.Month)]
+        [DataRow(BorderRange.Year)]
+        [DataRow(null)]
+        [DataTestMethod()]
+        public void GetHighBorderTest(BorderRange borderRange)
+        {
+            MyClass obj = new MyClass();
+
+            DateTime dateToTest = obj.GetHighBorder(DateTime.Now, borderRange);
+            Assert.IsNotNull(dateToTest);
+            switch (borderRange)
+            {
+                case BorderRange.Day:
+                    Assert.Equals(dateToTest, new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + 1));
+                    break;
+                case BorderRange.Month:
+                    Assert.Equals(dateToTest, new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, 0)); ;
+                    // Or, assuming the code is correct: 
+                    // Assert.ThrowsException<NotSupportedException>(() => { obj.GetHighBorder(DateTime.Now, borderRange); });
+                    break;
+                case BorderRange.Year:
+                    Assert.Equals(dateToTest, new DateTime(DateTime.Now.Year + 1, 0, 0)); ;
+                    break;
+            }
+        }
+
+        [DataRow(BorderRange.Day)]
+        [DataRow(BorderRange.Month)]
+        [DataRow(BorderRange.Year)]
+        [DataRow(null)]
+        [DataTestMethod()]
+        public void GetLowBorderTest(BorderRange borderRange)
+        {
+            MyClass obj = new MyClass();
+
+            DateTime dateToTest = obj.GetLowBorder(DateTime.Now, borderRange);
+            DateTime dateToTest2 = obj.GetLowBorder(new DateTime(1984, 4, 8), borderRange); 
+            Assert.IsNotNull(dateToTest);
+
+            switch (borderRange)
+            {
+                case BorderRange.Day:
+                    Assert.Equals(dateToTest, new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day));
+                    Assert.Equals(dateToTest2, new DateTime(1984, 4, 8));
+                    break;
+                case BorderRange.Month:
+                    Assert.Equals(dateToTest, new DateTime(DateTime.Now.Year, DateTime.Now.Month, 0));
+                    Assert.Equals(dateToTest2, new DateTime(1984, 4, 0));
+                    break;
+                case BorderRange.Year:
+                    Assert.Equals(dateToTest, new DateTime(DateTime.Now.Year, 0, 0));
+                    Assert.Equals(dateToTest2, new DateTime(1984, 0, 0));
+                    break;
+            }
+        }
+
+ 
+
+        //which test are necessary to 100% code coverage (test all the function)
+    }
+}
